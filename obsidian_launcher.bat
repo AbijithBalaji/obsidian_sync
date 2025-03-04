9a3bfe6dee9074238cdd2f5473d508b133aa1fc2
Abijith <abijith.balaji@gmail.com>
2025-03-04 19:59:44 +0530

    Auto-sync: latest updates

--- obsidian_launcher.bat
+++ obsidian_launcher.bat
@@ -1,3 +1,2 @@
 @echo off
-start /b pythonw "C:\Users\abiji\Personal\Project\Sync Project\obsidian_sync\obsidian_sync.py"
-start "" "C:\Users\abiji\AppData\Local\Programs\Obsidian\Obsidian.exe"
+pythonw "C:\Users\abiji\Personal\Project\Sync Project\obsidian_sync\obsidian_sync.py"
